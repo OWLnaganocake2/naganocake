b8cad255adadb80b020e56046c1d048aeb57639c
EC2 Default User <ec2-user@ip-172-31-46-129.ap-northeast-1.compute.internal>
2024-10-15 11:46:21 +0000

    [Add] 管理者側会員機能controller・顧客側top/aboutページの追加

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,16 @@
 Rails.application.routes.draw do
+
+  #顧客側
+  root to: "public/homes#top"
+  namespace :public do
+    get "home/about"=>"homes#about"
+  end
+
+  #管理者側
+  namespace :admin do
+    resources :customers, only: [:index, :show, :edit, :update]
+  end
+
   devise_for :customers
   devise_for :admins
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
