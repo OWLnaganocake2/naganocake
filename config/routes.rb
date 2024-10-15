294204a177fac047de7c5ccb54e5e9bf28289e4a
Yoshi1900 <177987679+Yoshi1900@users.noreply.github.com>
2024-10-15 11:14:47 +0000

    devise変更など

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,11 @@
 Rails.application.routes.draw do
-  devise_for :customers
-  devise_for :admins
+  devise_for :customers,controllers: {
+    sessions: 'public/sessions',
+    registrations: 'public/registrations'
+  }
+  
+  devise_for :admin,skip: [:registrations,:passwords], controllers: {
+    sessions: 'admin/sessions'
+  }
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
