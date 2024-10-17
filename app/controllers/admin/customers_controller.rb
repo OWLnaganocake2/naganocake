11b93f6a565cfb1a935a507d1c49fe078d299abe
Yoshi1900 <177987679+Yoshi1900@users.noreply.github.com>
2024-10-17 06:55:38 +0000

    DBのカラムの変更

--- app/controllers/admin/customers_controller.rb
+++ app/controllers/admin/customers_controller.rb
@@ -4,11 +4,5 @@ class Admin::CustomersController < ApplicationController
       @customers_all = Customer.all
       @customers = Customer.page(params[:page]).per(10)
     end
-  
-    def show
-    end
-
-    def edit
-    end
-
+    
 end
