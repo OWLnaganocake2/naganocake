3dc0f0e574eafa3c05767218bb47074178fdc49d
EC2 Default User <ec2-user@ip-172-31-16-243.ap-northeast-1.compute.internal>
2024-10-20 09:12:40 +0000

    注文情報入力画面

--- app/models/address.rb
+++ app/models/address.rb
@@ -1,7 +1,11 @@
 class Address < ApplicationRecord
   belongs_to :customer
-  
+
   validates :post_code, length: {is: 7}, numericality: { only_integer: true }, presence: true
   validates :address, presence: true
   validates :name, presence: true
+
+  def address_display #カリキュラム通りの記述
+    '〒' + postal_code + ' ' + address + ' ' + name
+  end
 end
