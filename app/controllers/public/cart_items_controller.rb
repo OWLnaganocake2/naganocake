e1ad0471e7a32133400913495090750fc9a0a8d4
Yoshi1900 <177987679+Yoshi1900@users.noreply.github.com>
2024-10-21 11:26:01 +0000

    オーダー合計金額など

--- app/controllers/public/cart_items_controller.rb
+++ app/controllers/public/cart_items_controller.rb
@@ -8,8 +8,8 @@ class Public::CartItemsController < ApplicationController
     def create
         @cart_item = current_customer.cart_items.new(cart_item_params)
       if current_customer.cart_items.find_by(item_id: params[:cart_item][:item_id]).present?
-                          #元々カート内にあるもの「item_id」　
-                          #今追加した　　　　　　　params[:cart_item][:item_id])
+                          #元々カート内にあるもの「item_id」
+                          #今追加したparams[:cart_item][:item_id])
         cart_item = current_customer.cart_items.find_by(item_id: params[:cart_item][:item_id])
         cart_item.amount += params[:cart_item][:amount].to_i#cart_item.quantityに今追加したparams[:cart_item][:quantity]を加える#.to_iとして数字として扱う
         cart_item.save
@@ -18,7 +18,7 @@ class Public::CartItemsController < ApplicationController
       elsif @cart_item.save
         @cart_items = current_customer.cart_items.all
         render 'index'
-      else　# 保存できなかった場合
+      else # 保存できなかった場合
         flash[:notice] = "カートの保存に失敗しました"
         render 'index'
       end
@@ -40,8 +40,8 @@ class Public::CartItemsController < ApplicationController
     def update
         @cart_item = CartItem.find(params[:id])
        if @cart_item.update(cart_item_params)
-         redirect_to cart_items_path(@cart_item)
-       　 flash[:notice] = "カートを更新しました！"
+         redirect_to cart_items_path
+         flash[:notice] = "カートを更新しました！"
        else
         flash[:notice] = "カートの更新に失敗しました！"  
         redirect_to cart_items_path(@cart_item)
