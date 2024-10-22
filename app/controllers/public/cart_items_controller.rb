c8c384d95ff47146779a951bebecc9f375b2d84f
EC2 Default User <ec2-user@ip-172-31-16-243.ap-northeast-1.compute.internal>
2024-10-22 10:28:37 +0000

    配送先一覧とマイページのbootstrapで整え済み

--- app/controllers/public/cart_items_controller.rb
+++ app/controllers/public/cart_items_controller.rb
@@ -4,7 +4,7 @@ class Public::CartItemsController < ApplicationController
     def index
         @cart_items = current_customer.cart_items.all
     end
-    
+
     def create
         @cart_item = current_customer.cart_items.new(cart_item_params)
       if current_customer.cart_items.find_by(item_id: params[:cart_item][:item_id]).present?
@@ -14,7 +14,7 @@ class Public::CartItemsController < ApplicationController
         cart_item.amount += params[:cart_item][:amount].to_i#cart_item.quantityに今追加したparams[:cart_item][:quantity]を加える#.to_iとして数字として扱う
         cart_item.save
         redirect_to cart_items_path
-        # もしカート内に「同じ」商品がない場合は通常の保存処理 
+        # もしカート内に「同じ」商品がない場合は通常の保存処理
       elsif @cart_item.save
         @cart_items = current_customer.cart_items.all
         render 'index'
@@ -23,18 +23,18 @@ class Public::CartItemsController < ApplicationController
         render 'index'
       end
     end
-    
+
     def destroy
         cart_item = CartItem.find(params[:id])
         cart_item.destroy
-        @cart_items = current_customer.cart_items.all
+        @cart_items = CartItem.all
         render 'index'
     end
 
     def destroy_all
         CartItem.destroy_all
         current_customer.cart_items.destroy_all
-        redirect_to cart_items_path, notice: 'カートが空になりました。' 
+        redirect_to cart_items_path, notice: 'カートが空になりました。'
     end
 
     def update
@@ -43,7 +43,7 @@ class Public::CartItemsController < ApplicationController
          redirect_to cart_items_path
          flash[:notice] = "カートを更新しました！"
        else
-        flash[:notice] = "カートの更新に失敗しました！"  
+        flash[:notice] = "カートの更新に失敗しました！"
         redirect_to cart_items_path(@cart_item)
        end
     end
