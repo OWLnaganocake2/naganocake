92c579e2791d42e67b192048f8b2fcf848396cb8
EC2 Default User <ec2-user@ip-172-31-16-243.ap-northeast-1.compute.internal>
2024-10-22 07:34:01 +0000

    配送先２番ログインしてる垢で登録した住所反映

--- app/controllers/public/orders_controller.rb
+++ app/controllers/public/orders_controller.rb
@@ -2,7 +2,7 @@ class Public::OrdersController < ApplicationController
     def new
         @order = Order.new
         @customer = current_customer
-        # @addresses = Address.where(customer_id: current_customer.id)
+        @addresses = current_customer.addresses
          sum = 0
          cart_items = current_customer.cart_items
         cart_items.each do |cart_item|
@@ -94,13 +94,19 @@ class Public::OrdersController < ApplicationController
         # 登録済住所が選択された時
         elsif destination == 1
 
-            address = Address.find(params[:order][:address_id])
-            @post_code = address.post_code
-            @address = address.address
-            @name = address.name
+            selected_address = current_customer.addresses.find_by(id: params[:order][:address_id])
+              if selected_address
+                @post_code = selected_address.post_code
+                @address = selected_address.address
+                @name = selected_address.name
+              else
+                flash.now[:alert] = '選択された住所が見つかりません。'
+                render :new
+              end
 
         # 新しいお届け先が選択された時
         elsif destination == 2
+
             @post_code = params[:order][:post_code]
             @address = params[:order][:address]
             @name = params[:order][:name]
