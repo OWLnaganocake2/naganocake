ea6f1165cc955bb9b8ab24f68eb67910b9cf66c4
Yoshi1900 <177987679+Yoshi1900@users.noreply.github.com>
2024-10-26 05:17:20 +0000

    最終修正

--- app/controllers/public/sessions_controller.rb
+++ app/controllers/public/sessions_controller.rb
@@ -1,6 +1,7 @@
 # frozen_string_literal: true
 
 class Public::SessionsController < Devise::SessionsController
+  before_action :reject_customer, only: [:create]
   # before_action :configure_sign_in_params, only: [:create]
 
   # GET /resource/sign_in
@@ -29,9 +30,9 @@ class Public::SessionsController < Devise::SessionsController
 
   # 会員の論理削除のための記述。退会後は、同じアカウントでは利用できない。
   def reject_customer
-    @customer = Customer.find_by(email: params[:customer][:name])
-    if @customer
-      if @customer.valid_password?(params[:customer][:password]) && (@customer.is_deleted == false)
+    @customer = Customer.find_by(email: params[:customer][:email])
+    if @customer.present?
+      if @customer.valid_password?(params[:customer][:password]) && (@customer.is_deleted == true)
         flash[:notice] = "退会済みです。再度ご登録をしてご利用ください。"
         redirect_to new_customer_registration_path
       else
