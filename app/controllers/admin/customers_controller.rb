0f99488364124c212404b0c6f83eed7555d3f8c9
Yoshi1900 <177987679+Yoshi1900@users.noreply.github.com>
2024-10-26 06:29:36 +0000

    adminの会員情報編集の修正

--- app/controllers/admin/customers_controller.rb
+++ app/controllers/admin/customers_controller.rb
@@ -19,6 +19,17 @@ class Admin::CustomersController < ApplicationController
       redirect_to admin_genres_path
     end
     
+    def update
+      @customer = Customer.find(params[:id]) 
+       if @customer.update(customer_params)
+        redirect_to admin_customer_path(@customer)
+        flash[:notice] = "商品情報を更新しました！"
+       else
+        flash[:notice] = "商品情報の更新に失敗しました！"  
+        render :edit
+       end
+    end
+    
        private
     
     def customer_params
